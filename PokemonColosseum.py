8bf1ae67f2662649426bcbea54c96754877d78f6
Eric Stevens Jr. <ericstevensjrprofessional@outlook.com>
2024-02-02 20:30:41 -0500

    Working copy of my project. Still to to implement some quality control with try-excepts for bad user inputs, and an opportunity to play again based on user inputs. That will be done next.

--- PokemonColosseum.py
+++ PokemonColosseum.py
@@ -32,39 +32,54 @@ gameOver = False
 while gameOver == False:
     currentPlayerPokemon = playerTeam.pokemon[0]
     currentRocketPokemon = teamRocket.pokemon[0]
+
     if currentMove == playerTeam.name:
-        print(f"{currentPlayerPokemon.name}'s available moves:")
+        print(f"\nChoose the move for {currentPlayerPokemon.name}")
         for i, move in enumerate(currentPlayerPokemon.moves, start = 1):
             print(f"{i}. {move.name}")
-        moveSelection = int(input("Choose your move: ")) - 1
-        selectedMove = currentPlayerPokemon.moves[moveSelection]
+
+        print("\n")
+
+        moveSelection = int(input()) - 1
+        print(f"\n{playerTeam.name}'s choice: {(moveSelection + 1)}\n")
+        selectedMove = currentPlayerPokemon.moves.pop(moveSelection)
+        if not currentPlayerPokemon.moves:
+            currentPlayerPokemon.resetMoves()
+
         damageNum = damage(selectedMove, currentPlayerPokemon, currentRocketPokemon)
         print(f"{playerTeam.name}'s {currentPlayerPokemon.name} cast '{selectedMove.name}' to {currentRocketPokemon.name}")
         print(f"Damage to {currentRocketPokemon.name} is {damageNum} points.")
+
         currentRocketPokemon.hp = currentRocketPokemon.hp - damageNum
         if currentRocketPokemon.checkHp() is False:
             print(f"Now {currentRocketPokemon.name} faints back to poke ball., and {currentPlayerPokemon.name} has {currentPlayerPokemon.hp} HP.")
             teamRocket.pokemon.pop(0)
             if not teamRocket.pokemon:
-                print(f"All of Team Rocket's Pokemon fainted, and {playerTeam.name} prevails!")
+                print(f"\nAll of Team Rocket's Pokemon fainted, and {playerTeam.name} prevails!")
                 gameOver = True
-            else: 
-                print(f"Now {currentRocketPokemon.name} has {currentRocketPokemon.hp} HP, and {currentPlayerPokemon.name} has {currentPlayerPokemon.hp} HP")
-        currentMove = teamRocket.name
+            else:
+                print(f"\nNext for Team Rocket, {teamRocket.pokemon[0].name} enters battle!\n")
+        else: 
+            print(f"Now {currentRocketPokemon.name} has {currentRocketPokemon.hp} HP, and {currentPlayerPokemon.name} has {currentPlayerPokemon.hp} HP")
+            currentMove = teamRocket.name
         pass
 
     else:
         selectedMove = teamRocket.selectTeamRocketMove(currentRocketPokemon)
+
         damageNum = damage(selectedMove, currentRocketPokemon, currentPlayerPokemon)
-        print(f"Team Rocket's {currentRocketPokemon.name} cast '{selectedMove.name}' to {currentPlayerPokemon.name}")
+        print(f"\nTeam Rocket's {currentRocketPokemon.name} cast '{selectedMove.name}' to {currentPlayerPokemon.name}")
         print(f"Damage to {currentPlayerPokemon.name} is {damageNum} points.")
+
         currentPlayerPokemon.hp = currentPlayerPokemon.hp - damageNum
         if currentPlayerPokemon.checkHp() is False:
             print(f"Now {currentRocketPokemon.name} has {currentRocketPokemon.hp} HP, and {currentPlayerPokemon.name} faints back to poke ball.")
             playerTeam.pokemon.pop(0)
             if not playerTeam.pokemon:
-                print(f"All of Team {playerTeam.name}'s Pokemon fainted, and Team Rocket prevails!")
+                print(f"\nAll of Team {playerTeam.name}'s Pokemon fainted, and Team Rocket prevails!")
                 gameOver = True
+            else:
+                print(f"\nNext for {playerTeam.name}, {playerTeam.pokemon[0].name} enters battle!\n")
         else: 
             print(f"Now {currentRocketPokemon.name} has {currentRocketPokemon.hp} HP, and {currentPlayerPokemon.name} has {currentPlayerPokemon.hp} HP")
 
